62d3440551ffceddb90d8cfab5cee6607edd83ac
luciano <lucho.2012.tandil@gmail.com>
2025-07-01 00:36:01 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<SeccionFooter />
+<CustomCursor :config='{ highlighted_tags: [ "a", "button", ".text-res-1", ".circ-met", ".text-effect", ".menu-item" ], 
+                            transform_time: ".2s", click_effect_enabled: false }' />
+<WhatsAppWidget phone="542494279833" :quick_replies="quick_replies" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const quick_replies = ref([
+  {
+    text: "Desarrollo de APP's",
+    phone: "542494279833"
+  },
+  {
+    text: "Testing",
+    phone: "542494067516"
+  },
+])
+
+import SeccionFooter from './components/SeccionFooter.vue'
+</script>
+
---
+++ src/components/SeccionFooter.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="row app-footer pt-2">
+  <div class="p-4 container app-footer-container">
+    
+    <div class="row justify-content-center alig-items-center">
+      <div class="col-12 col-sm-6 text-center disable-select">
+        <p><span class="copyleft">&copy;</span> 2025 Greenborn - Desarrollo de Software</p>
+      </div>
+
+      <div class="col-12 col-sm-6">
+        <div class="row">
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style>
+.img-contact{
+  width: 3rem;
+  height: auto;
+}
+
+.app-footer{
+  border-top: .1rem solid #333;
+  background: #000;
+  color: #fff;
+}
+</style>
