84b352b84610a00f2a9a3594860789fd1ff5bb60
luciano <lucho.2012.tandil@gmail.com>
2025-08-10 15:45:54 -0300

    Mejora visual: Anexo Metodológico con icono informativo y descripción

--- src/components/SeccionOpenData.vue
+++ src/components/SeccionOpenData.vue
@@ -10,6 +10,33 @@
 
         <div class="row justify-content-center align-items-center mt-5">
             <div class="col-auto">
+                <h4 class="mb-3 text-center">Julio 2025</h4>
+                <div class="d-flex gap-4 flex-wrap">
+                    <div class="p-2 border rounded text-center bg-light">
+                        <a href="https://drive.google.com/file/d/1xn2AzLMtTSyjMYh9jVODtCJOXpU4oTtx/view?usp=drive_link" target="_blank" class="text-decoration-none text-dark">
+                            <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                            <div>Listado Precios<br><b>Julio 2025</b> .csv</div>
+                        </a>
+                    </div>
+                    <div class="p-2 border rounded text-center bg-light">
+                        <a href="https://drive.google.com/file/d/16KjRuOuWzMoUX0pzLJtj7oy6JyANZhBw/view?usp=sharing" target="_blank" class="text-decoration-none text-dark">
+                            <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                            <div>Listado Precios<br><b>Julio 2025</b> .ods</div>
+                        </a>
+                    </div>
+                    <div class="p-2 border rounded text-center bg-light">
+                        <a href="https://github.com/Greenborn/precios/blob/dev/back/informes/metodologia_exportacion_precios.md" target="_blank" class="text-decoration-none text-dark">
+                            <img src="/img/info.svg" alt="Info" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                            <div><b>Anexo Metodológico</b></div>
+                            <div class="small text-muted">Documento explicativo sobre la metodología utilizada</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center align-items-center mt-5">
+            <div class="col-auto">
                 <h4 class="mb-3 text-center">Junio 2025</h4>
                 <div class="d-flex gap-4 flex-wrap">
                     <div class="p-2 border rounded text-center bg-light">
