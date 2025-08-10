f7584fe2527a0e25397906db937728df1bab9767
luciano <lucho.2012.tandil@gmail.com>
2025-08-10 16:43:29 -0300

    Mejora responsiva: cards de descargas ahora adaptables a móviles y escritorio

--- src/components/SeccionOpenData.vue
+++ src/components/SeccionOpenData.vue
@@ -11,67 +11,81 @@
         <div class="row justify-content-center align-items-center mt-5">
             <div class="col-auto">
                 <h4 class="mb-3 text-center">Julio 2025</h4>
-                <div class="d-flex gap-4 flex-wrap">
-                    <div class="p-2 border rounded text-center bg-light">
+                <div class="row g-3">
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://drive.google.com/file/d/1Faf_6iOkyiPWZnM_42ecRtW1ogFDfAz6/view?usp=sharing" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Julio 2025</b> .csv</div>
                         </a>
                     </div>
-                    <div class="p-2 border rounded text-center bg-light">
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://drive.google.com/file/d/1YstywP59smq12e7zhg3s7VGkIBRIMTkw/view?usp=sharing" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Julio 2025</b> .ods</div>
                         </a>
                     </div>
-                    <div class="p-2 border rounded text-center bg-light">
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://github.com/Greenborn/precios/blob/dev/back/informes/metodologia_exportacion_precios.md" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/info.svg" alt="Info" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div><b>Anexo Metodológico</b></div>
                             <div class="small text-muted">Documento explicativo sobre la metodología utilizada</div>
                         </a>
                     </div>
-                </div>
+            </div>
+        </div>
             </div>
         </div>
 
         <div class="row justify-content-center align-items-center mt-5">
             <div class="col-auto">
                 <h4 class="mb-3 text-center">Junio 2025</h4>
-                <div class="d-flex gap-4 flex-wrap">
-                    <div class="p-2 border rounded text-center bg-light">
+                <div class="row g-3">
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://drive.google.com/file/d/1iDw_fgHDNLcy34bvemANjWxlWDP_Fw_A/view?usp=drive_link" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Junio 2025</b> .csv</div>
                         </a>
                     </div>
-                    <div class="p-2 border rounded text-center bg-light">
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://drive.google.com/file/d/1yWaPYoCnWq8vAzN9DDQ7PKkVamfvUY3o/view?usp=drive_link" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Junio 2025</b> .ods</div>
                         </a>
                     </div>
-                </div>
+            </div>
+        </div>
             </div>
         </div>
 
         <div class="row justify-content-center align-items-center mt-5">
             <div class="col-auto">
                 <h4 class="mb-3 text-center">Año 2024</h4>
-                <div class="d-flex gap-4 flex-wrap">
-                    <div class="p-2 border rounded text-center bg-light">
+                <div class="row g-3">
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://drive.google.com/file/d/1CS2vp7jf1Y8-bbjzeFREPwkQPS7vjUHv/view?usp=drive_link" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Año 2024</b> .csv</div>
                         </a>
                     </div>
-                    <div class="p-2 border rounded text-center bg-light">
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <div class="p-2 border rounded text-center bg-light h-100">
                         <a href="https://docs.google.com/spreadsheets/d/1D54HucLxTYQthh2c9ueH2Y0kOeoOCzIa/edit?usp=drive_link&ouid=101735906968288029127&rtpof=true&sd=true" target="_blank" class="text-decoration-none text-dark">
                             <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                             <div>Listado Precios<br><b>Año 2024</b> .ods</div>
                         </a>
                     </div>
-                </div>
+            </div>
+        </div>
             </div>
         </div>
     </div>
