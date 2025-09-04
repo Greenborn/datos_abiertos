7124abdd059cf96b064c8f12a7976de59185160c
luciano <lucho.2012.tandil@gmail.com>
2025-09-04 20:43:41 -0300

    [0.1.5]

--- src/components/SeccionOpenData.vue
+++ src/components/SeccionOpenData.vue
@@ -3,8 +3,51 @@
         <div class="row justify-content-center align-items-center mt-5">
             <div class="col col-md-auto">
                 <h2 class="display-5 display-md-3 text-center mb-4">Relevamiento de Precios</h2>
-                <p>A continuación puede descargar el compilado de precios registrados en 
-                    <a href="https://precios.greenborn.com.ar" target="_blank">https://precios.greenborn.com.ar</a></p>
+                <p>A continuación puede descargar el compilado de precios registrados en
+                    <a href="https://precios.greenborn.com.ar" target="_blank">https://precios.greenborn.com.ar</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="row justify-content-center align-items-center mt-5">
+            <div class="col-auto">
+                <h4 class="mb-3 text-center">Agosto 2025</h4>
+                <div class="row g-3">
+                    <div class="col-12 col-md-auto">
+                        <a href="https://drive.google.com/file/d/13IRIs0foHmlE7M6ZkcbB2-qpRhtR_E0b/view?usp=drive_link"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
+                            <div class="p-2 border rounded text-center bg-light h-100">
+                                <img src="/img/csv.svg" alt="CSV"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <div>Listado Precios<br><b>Agosto 2025</b> .csv</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <a href="https://drive.google.com/file/d/1vOqzEXSDTNK5C4eRIktryN2BEBsogA5R/view?usp=drive_link"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
+                            <div class="p-2 border rounded text-center bg-light h-100">
+                                <img src="/img/ods.svg" alt="ODS"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <div>Listado Precios<br><b>Agosto 2025</b> .ods</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-auto">
+                        <a href="https://github.com/Greenborn/precios/blob/dev/back/informes/metodologia_exportacion_precios.md"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
+                            <div class="p-2 border rounded text-center bg-light h-100">
+                                <img src="/img/info.svg" alt="Info"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <div><b>Anexo Metodológico</b></div>
+                                <div class="small text-muted">Documento explicativo sobre la metodología utilizada</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -13,31 +56,40 @@
                 <h4 class="mb-3 text-center">Julio 2025</h4>
                 <div class="row g-3">
                     <div class="col-12 col-md-auto">
-                        <a href="https://drive.google.com/file/d/1Faf_6iOkyiPWZnM_42ecRtW1ogFDfAz6/view?usp=sharing" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://drive.google.com/file/d/1Faf_6iOkyiPWZnM_42ecRtW1ogFDfAz6/view?usp=sharing"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/csv.svg" alt="CSV"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Julio 2025</b> .csv</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-12 col-md-auto">
-                        <a href="https://drive.google.com/file/d/1YstywP59smq12e7zhg3s7VGkIBRIMTkw/view?usp=sharing" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://drive.google.com/file/d/1YstywP59smq12e7zhg3s7VGkIBRIMTkw/view?usp=sharing"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/ods.svg" alt="ODS"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Julio 2025</b> .ods</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-12 col-md-auto">
-                        <a href="https://github.com/Greenborn/precios/blob/dev/back/informes/metodologia_exportacion_precios.md" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://github.com/Greenborn/precios/blob/dev/back/informes/metodologia_exportacion_precios.md"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/info.svg" alt="Info" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/info.svg" alt="Info"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div><b>Anexo Metodológico</b></div>
                                 <div class="small text-muted">Documento explicativo sobre la metodología utilizada</div>
                             </div>
                         </a>
                     </div>
-        </div>
+                </div>
             </div>
         </div>
 
@@ -46,22 +98,28 @@
                 <h4 class="mb-3 text-center">Junio 2025</h4>
                 <div class="row g-3">
                     <div class="col-12 col-md-auto">
-                        <a href="https://drive.google.com/file/d/1iDw_fgHDNLcy34bvemANjWxlWDP_Fw_A/view?usp=drive_link" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://drive.google.com/file/d/1iDw_fgHDNLcy34bvemANjWxlWDP_Fw_A/view?usp=drive_link"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/csv.svg" alt="CSV"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Junio 2025</b> .csv</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-12 col-md-auto">
-                        <a href="https://drive.google.com/file/d/1yWaPYoCnWq8vAzN9DDQ7PKkVamfvUY3o/view?usp=drive_link" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://drive.google.com/file/d/1yWaPYoCnWq8vAzN9DDQ7PKkVamfvUY3o/view?usp=drive_link"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/ods.svg" alt="ODS"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Junio 2025</b> .ods</div>
                             </div>
                         </a>
                     </div>
-        </div>
+                </div>
             </div>
         </div>
 
@@ -70,26 +128,32 @@
                 <h4 class="mb-3 text-center">Año 2024</h4>
                 <div class="row g-3">
                     <div class="col-12 col-md-auto">
-                        <a href="https://drive.google.com/file/d/1CS2vp7jf1Y8-bbjzeFREPwkQPS7vjUHv/view?usp=drive_link" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://drive.google.com/file/d/1CS2vp7jf1Y8-bbjzeFREPwkQPS7vjUHv/view?usp=drive_link"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/csv.svg" alt="CSV" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/csv.svg" alt="CSV"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Año 2024</b> .csv</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-12 col-md-auto">
-                        <a href="https://docs.google.com/spreadsheets/d/1D54HucLxTYQthh2c9ueH2Y0kOeoOCzIa/edit?usp=drive_link&ouid=101735906968288029127&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-dark d-block h-100">
+                        <a href="https://docs.google.com/spreadsheets/d/1D54HucLxTYQthh2c9ueH2Y0kOeoOCzIa/edit?usp=drive_link&ouid=101735906968288029127&rtpof=true&sd=true"
+                            target="_blank" rel="noopener noreferrer"
+                            class="text-decoration-none text-dark d-block h-100">
                             <div class="p-2 border rounded text-center bg-light h-100">
-                                <img src="/img/ods.svg" alt="ODS" style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
+                                <img src="/img/ods.svg" alt="ODS"
+                                    style="width:2em;height:2em;vertical-align:middle;margin-bottom:0.5em;" />
                                 <div>Listado Precios<br><b>Año 2024</b> .ods</div>
                             </div>
                         </a>
                     </div>
-        </div>
+                </div>
             </div>
         </div>
     </div>
-    
+
 </template>
 
 <script setup>
